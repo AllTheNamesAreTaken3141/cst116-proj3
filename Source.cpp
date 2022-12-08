033c4cc0ef24c9a4553c0e5274fad3a321e23b68
Joseph Williams <joe.williams@oit.edu>
2022-12-07 16:11:26 -0800

    added pseudocode and output files. also spend way too long rewriting getNewMatrix()

--- Source.cpp
+++ Source.cpp
@@ -1,110 +1,25 @@
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 #include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
-using std::cout;
-using std::cin;
-using std::endl;
-
-// I stole this from the internet and I have basically no idea how it works,
-// but it lets you cout to both the console and a text file at the same time,
-// and all you have to do is pass everything to the Tee object instead of cout.
-// Very useful since now I don't have to copy-paste the console every time I need to store my output somewhere.
-class Tee
-{
-private:
-    std::ostream& os1, & os2;
-public:
-    Tee(std::ostream& os1, std::ostream& os2) : os1(os1), os2(os2) {}
-    template <typename T>
-    Tee& operator<<(const T& thing)
-    {
-        os1 << thing;
-        os2 << thing;
-
-        return *this;
-    }
-
-
-    // function that takes a custom stream, and returns it
-    typedef Tee& (*TeeManipulator)(Tee&);
-
-    // take in a function with the custom signature
-    Tee& operator<<(TeeManipulator manip)
-    {
-        // call the function, and return it's value
-        return manip(*this);
-    }
-
-    // define the custom endl for this stream.
-    // note how it matches the `TeeManipulator`
-    // function signature
-    static Tee& endl(Tee& stream)
-    {
-        // print a new line
-        stream << std::endl;
-
-        // do other stuff with the stream
-        // std::cout, for example, will flush the stream
-        stream << "Called Tee::endl!" << std::endl;
-
-        return stream;
-    }
-
-    // this is the type of std::cout
-    typedef std::basic_ostream<char, std::char_traits<char> > CoutType;
-
-    // this is the function signature of std::endl
-    typedef CoutType& (*StandardEndLine)(CoutType&);
-
-    // define an operator<< to take in std::endl
-    Tee& operator<<(StandardEndLine manip)
-    {
-        // call the function, but we cannot return its value
-        manip(std::cout);
-
-        return *this;
-    }
-};
 
 // Here's how I'm storing each matrix.
-vector<vector<int> > m1 = {
-    {-57, 82, 43},
-    {17, -93, 89},
-    {-2, 53, 17}
-};
-vector<vector<int> > m2 = {
-    {-23, 87, -3},
-    {-5, 72, 50},
-    {59, 29, -35}
-};
-//Used to assign product and sum
-vector<vector<int>> total = {
-    {0,0,0},
-    {0,0,0},
-    {0,0,0}
-};
+vector<vector<int> > m1 = {};
+vector<vector<int> > m2 = {};
+vector<vector<int>> total = {};
 
 int dotProduct(vector<int> r1, vector<int> r2);
 void displayMenu();
 void processMenuChoice(int menuChoice);
 void printVector(vector<vector<int>> m);
-vector<vector<int>> getNewMatrix();
-vector<vector<int>> addMatrix(vector<vector<int>> m1, vector<vector<int>> m2);
+vector<vector<int> > getNewMatrix();
+vector<vector<int> > addMatrix(vector<vector<int>> m1, vector<vector<int>> m2);
 vector<vector<int> > multiplyMatrix(vector<vector<int> > m1, vector<vector<int> > m2);
 
 int main()
 {
-    // The path to the log file. The ./ makes it a relative path,
-    // so it starts at the root folder of the project.
-    // If you right-click on the very top file in the Solution Explorer
-    // and click "Open folder in File Explorer", it should take you there.
-    // IF YOU WANT TO SAVE A SECOND LOG FILE, YOU MUST CHANGE THIS FILENAME.
-    // If you don't change it and run the code a second time,
-    // it will destroy the first log.
-    ofstream logFile("./logs/test-log.txt");
-    Tee teeOut(std::cout, logFile); // Creates a new Tee named teeOut. Now all you have to do is use teeOut instead of cout (so, "cout << "Hello, world!" << endl;" becomes "teeOut << "Hello, world!" << endl;").
     cout << "Please input data for the first matrix."<< endl;
     m1 = getNewMatrix();
     cout << endl<< "Please input data for the second matrix."<< endl;
@@ -118,82 +33,141 @@ int main()
         cout << endl;
     }
     return 0;
-    
-    
-    logFile.close(); // Closes the log file because you should always close your files. Make sure this is the very last line in main.
 }
+
 //Prints out all menu options for the user to choose from
 void displayMenu() {
-    cout << "What do you want to do with your matrixes?" << endl
+    cout << "What do you want to do with your matrices?" << endl
         << "1. Enter a new matrix 1." << endl
         << "2. Enter a new matrix 2." << endl
-        << "3. Add matrixes." << endl
-        << "4. Multiply matrixes" << endl
+        << "3. Add matrices." << endl
+        << "4. Multiply matrices" << endl
         << "5. End program."<< endl;
 }
-//Cycles through each spot in the matrix and asks the user to assign a value to fill the 3x3 matrix.
+
+// It took me two hours and a ton of spaghetti code
+// just to let the user input an entire row at once.
+// Worth it, though.
 vector<vector<int>> getNewMatrix() {
-    vector<vector<int>> m3 = {};
+    vector<vector<int>> matrix = {};
     vector<int> row = {};
-    int temp;
+    string temp = "";
+    string rowString = "";
+    int cell;
+    bool errorFlag = false; // Used to break (actually to not break) a loop from inside another loop.
     for (int r = 0; r < 3; r++)
     {
-        row = {};
-        for (int c = 0; c < 3; c++)
+        while (true)
         {
-            cout << "What do you want to put in row " << r << " column " << c << "? ";
-            cin >> temp;
-            row.push_back(temp);
+            errorFlag = false;
+            cout << "Enter the " << (r == 0 ? "first" : r == 1 ? "second" : "third") << " row of your matrix. Separate numbers with spaces: ";
+            row = {};
+
+            // I still don't like that it takes this much code
+            // just to get input that includes whitespace.
+            cin >> rowString;
+            getline(cin, temp);
+            rowString += temp;
+            
+            // At least 90% of my pain writing this was trying to get cin to loop
+            // until it runs out of input, but it turns out that that's kind of impossible.
+            // I think it's because when cin is empty it just waits for more input
+            // instead of setting eof() to true, but either way it causes an infinite loop.
+            // Thankfully, putting the input into a different stringstream seems to solve the problem.
+            istringstream stream(rowString);
+            while (!stream.eof())
+            {
+                stream >> cell;
+
+                // If stream >> cell fails (because the user didn't input a number)
+                // then stream sets its failbit to true.
+                if (stream.fail())
+                {
+                    cout << "You need to enter numbers!" << endl;
+                    errorFlag = true;
+                    break;
+                }
+                else
+                {
+                    row.push_back(cell);
+                }
+            }
+
+            // errorFlag is set to true if the user didn't enter numbers,
+            // so all of this can be skipped.
+            if (!errorFlag)
+            {
+                // Makes sure the user entered at least 3 numbers
+                if (size(row) < 3)
+                {
+                    cout << "You need to enter 3 numbers!" << endl;
+
+                }
+                else
+                {
+                    // Truncate the row to 3 numbers in case the user entered more than 3.
+                    // If the user only entered 3, it does nothing and leaves the row unchanged.
+                    row.resize(3);
+                    matrix.push_back(row);
+                    break;
+                }
+            }
         }
-        m3.push_back(row);
     }
-    return m3;
+    return matrix;
 }
-//Takes the menu choice given by the user and either assigns new values for each matrix, adds the matrixes and assigns the sum to total,
-//multiplies the matrixes and assigns the product to total or ends breaks the while loop in the main function to end the program.
+
+// Takes the menu choice given by the user and
+// either assigns new values for each matrix,
+// adds the matrices and assigns the sum to total,
+// multiplies the matrices and assigns the product to total 
+// or breaks the while loop to end the program.
 void processMenuChoice(int menuChoice) {
     switch (menuChoice) {
     case 1:
         m1 = getNewMatrix();
         break;
     case 2:
-        m2 = getNewMatrix();;
+        m2 = getNewMatrix();
         break;
     case 3:
         total=addMatrix(m1,m2);
-        cout << "Adding Matrixes:" << endl;
+        cout << "Adding Matrices:" << endl;
         printVector(total);
         break;
     case 4:
         total=multiplyMatrix(m1,m2);
-        cout << "Multiplying Matrixes:" << endl;
+        cout << "Multiplying Matrices:" << endl;
         printVector(total);
         break;
     case 5:
         break;
     }
 }
-//Prints the matrix in a 3x3 format, used to nicely show products and sums.
+
+// Prints the matrix in a 3x3 format, used to nicely show products and sums.
 void printVector(vector<vector<int>> m) {
     for (int r = 0; r < size(m); r++) {
         for (int c = 0; c < size(m); c++) {
-            cout << m[r][c]<< ", ";
+            cout << setw(7) << right << (to_string(m[r][c]) + ", ");
         }
         cout << endl;
     }
 }
-//Part of the multiplying matrixes step. Gets the row of the first matrix and the corresponding collumn of the second matrix and returns the value
-//to the multiply matrix function.
+
+// Part of the multiplying matrices step. Gets the row of the first matrix
+// and the corresponding column of the second matrix and returns the dot product
 int dotProduct(vector<int> r, vector<int> c)
 {
-    int p=0;
-    for (int i = 0; i < size(r); i++)
+    int p = 0;
+    for (int i = 0; (i < size(r) && i < size(c)); i++)
     {
         p += r[i]* c[i];
     }
     return p;
 }
-//Just as the name suggests, this function adds the two matrixes together into a newly defined matrix and returns the matrix.
+
+// Just as the name suggests, this function adds the two matrices together into a newly defined matrix and returns the matrix.
 vector<vector<int>> addMatrix(vector<vector<int>> m1, vector<vector<int>> m2) {
     
     vector<vector<int>> m3 = {};
@@ -209,8 +183,10 @@ vector<vector<int>> addMatrix(vector<vector<int>> m1, vector<vector<int>> m2) {
     }
     return m3;
 }
-//The second part of the multiplyMatrix function where is seperates the matrixes into rows and columns and sends those to
-//the dotproduct function for the proper algorithm and returns it here where it's put into a seperate matrix and returned.
+
+// Multiplies two matrices together.
+// Takes the two matrices to be multiplied
+// and returns the product.
 vector<vector<int> > multiplyMatrix(vector<vector<int> > m1, vector<vector<int> > m2)
 {
     vector<vector<int> > rows = { m1[0], m1[1], m1[2] };
